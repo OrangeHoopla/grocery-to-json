4044baa8231fb3f2b1ee1ffec631da239554504e
Your Name <quadekirby@gmail.com>
2025-07-27 07:53:06 -0400

    added exif processing to tesseract proc

--- src/image_processors/tesseract_processor.rs
+++ src/image_processors/tesseract_processor.rs
@@ -1,4 +1,4 @@
-use image::{DynamicImage, ImageReader};
+use image::{DynamicImage, ImageDecoder, ImageReader};
 use rusty_tesseract::{Args, Image};
 
 
@@ -8,9 +8,12 @@ pub fn hello() {
 
 pub fn result(image: String) -> String {
 
-    let dynamic_image: DynamicImage = ImageReader::open(format!("images/{}",image))
-    .unwrap()
-    .with_guessed_format().unwrap().decode().unwrap();
+    //this should go to generic preproc and pass dynamic image in
+    let mut decoder = ImageReader::open(format!("images/{}",image)).unwrap()
+        .with_guessed_format().unwrap().into_decoder().unwrap();
+        let orientation = decoder.orientation().unwrap();
+        let mut dynamic_image = DynamicImage::from_decoder(decoder).unwrap();
+        dynamic_image.apply_orientation(orientation);
 
     let img = Image::from_dynamic_image(&dynamic_image).unwrap();
     
