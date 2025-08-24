3d9738daec9e08bac5dbbdb699a2798bcb51eda3
Your Name <quadekirby@gmail.com>
2025-08-23 22:10:51 -0400

    test

--- src/integration_test.rs
+++ src/integration_test.rs
@@ -2,8 +2,12 @@
 
 #[cfg(test)]
 mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
-    use super::*;
+    use std::fs;
+
+    use image::{DynamicImage, ImageDecoder, ImageReader};
+
+    use crate::image_processors::sobel_transform;
+
 
     #[test]
     fn test_add() {
@@ -12,8 +16,27 @@ mod tests {
 
     #[test]
     fn test_bad_add() {
-        // This assert would fire and test will fail.
-        // Please note, that private functions can be tested too!
-        assert_eq!(44,44);
+
+        
+        let result = sobel_transform::process_frame("images/IMG_5722.jpg".to_string(),"images/result.jpeg".to_string(),1);
+
+        let _ = result.save("result.jpeg");
+        let fer = rusty_tesseract::Image::from_dynamic_image(&result).unwrap();
+        let default_args = rusty_tesseract::Args::default();
+        let output = rusty_tesseract::image_to_string(&fer, &default_args).unwrap();
+        fs::write("edit.txt", output).expect("Should be able to write to `/foo/tmp`");
+        
+
+        // //original
+        let mut decoder = ImageReader::open("images/IMG_5722.jpg").unwrap()
+        .with_guessed_format().unwrap().into_decoder().unwrap();
+        let orientation = decoder.orientation().unwrap();
+        let mut dynamic_image = DynamicImage::from_decoder(decoder).unwrap();
+        dynamic_image.apply_orientation(orientation);
+
+        let img = rusty_tesseract::Image::from_dynamic_image(&dynamic_image).unwrap();
+        let default_args = rusty_tesseract::Args::default();
+        let output = rusty_tesseract::image_to_string(&img, &default_args).unwrap();
+        fs::write("original.txt", output).expect("Should be able to write to `/foo/tmp`");
     }
 }
