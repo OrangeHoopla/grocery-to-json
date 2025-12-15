912f5b4c3c7b29af5521fbe40030c84f332c019e
OrangeHoopla <quadekirby@gmail.com>
2025-12-14 20:48:02 -0500

    Basic Reciept structure steup

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,7 @@
-use clap::Parser;
-use std::fs;
+use image::ImageReader;
+use grocery_to_json::reciept::Reciept;
 
-///
-// #[derive(Parser, Debug)]
-// #[command(version, about, long_about = None)]
-// struct Args {
-//     /// Name of the person to greet
-//     #[arg(short='i', long, default_value_t = String::from("images/sample.jpg"))]
-//     image: String,
 
-//     /// Number of times to greet
-//     #[arg(short, long, default_value_t, value_enum)]
-//     textprocessor: TextProcessor,
-
-//     #[arg(short, long, default_value_t, value_enum)]
-//     imageprocessor: ImageProcessor,
-
-//     #[arg(short='o', long, default_value_t = String::from("result.jpg"))]
-//     output: String,
-// }
 
 fn main() {
     // hough_transform::example("images/sample.jpg");
@@ -27,5 +10,13 @@ fn main() {
     // let answer:Reciept = get_reciept_object(&args,raw_string);
     // let v = serde_json::to_string(&answer).unwrap();
     // fs::write(args.output, v).expect("Failed to write to designated file");
+// <ImageReader<std::io::BufReader<fs::File>>, std::io::Error>
+    let test: Reciept = ImageReader::open("./test.jpg")
+    .unwrap()
+    .try_into()
+    .unwrap();
+
+    println!("Hello world");
+    
 }
 
---
+++ src/reciept.rs
@@ -0,0 +1,33 @@
+use std::fs;
+
+use image::{DynamicImage, ImageDecoder, ImageReader};
+
+#[derive(Debug, PartialEq)]
+pub struct Reciept {
+
+    pub image: DynamicImage,
+    pub text: String, 
+
+}
+
+impl TryFrom<ImageReader<std::io::BufReader<fs::File>>> for Reciept {
+    type Error = ();
+
+    fn try_from(value: ImageReader<std::io::BufReader<fs::File>>) -> Result<Self, Self::Error> {
+        
+        let mut decoder = value
+        .with_guessed_format()
+        .unwrap()
+        .into_decoder()
+        .unwrap();
+
+        let orientation = decoder.orientation().unwrap();
+
+        let mut image = DynamicImage::from_decoder(decoder).unwrap();
+        image.apply_orientation(orientation);
+
+
+        Ok(Reciept{ image: image, text: "".to_owned() })
+
+    }
+}
