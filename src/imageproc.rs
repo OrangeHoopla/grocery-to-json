ea691ba6f0afe3a66d7e303dc1db8892f200c92e
Your Name <quadekirby@gmail.com>
2026-01-01 12:08:06 -0500

    completed hough transform of image and crop

--- src/imageproc.rs
+++ src/imageproc.rs
@@ -1,17 +1,22 @@
+use std::cmp;
+
 use crate::reciept::Reciept;
-use image::Rgb;
-use imageproc::hough::draw_polar_lines;
+use image::imageops;
 use imageproc::{
-    contrast::{otsu_level, threshold, ThresholdType},
+    contrast::{ThresholdType, otsu_level, threshold},
     edges::canny,
-    hough::{detect_lines, LineDetectionOptions, PolarLine},
-    morphology::{grayscale_open, Mask},
+    hough::{LineDetectionOptions, PolarLine, detect_lines},
+    morphology::{Mask, grayscale_open},
 };
 
 pub trait ImageProc {
     fn can(&mut self);
     fn crop_gray(&mut self);
-    fn otsu(&mut self);
+    fn otsu(&mut self,kernel_size: u8);
+    fn intersection_points(
+    line: PolarLine,
+    image_width: u32,
+    image_height: u32,) -> Option<((f32, f32), (f32, f32))>;
 }
 
 impl ImageProc for Reciept {
@@ -23,36 +28,34 @@ impl ImageProc for Reciept {
         self.image = inter;
     }
 
-    fn otsu(&mut self) {
+    fn otsu(&mut self,kernel_size: u8) {
         let gray = &self.image.grayscale();
         let otsu = otsu_level(&gray.to_luma8());
         let thres = threshold(&gray.to_luma8(), otsu, ThresholdType::Binary);
-        let morph = grayscale_open(&thres, &Mask::square(1));
+        let morph = grayscale_open(&thres, &Mask::square(kernel_size));
         self.image = image::DynamicImage::ImageLuma8(morph);
     }
 
     fn crop_gray(&mut self) {
-        let gray = &self.image.grayscale();
+        let gray = self.image.grayscale();
+        let otsu = otsu_level(&gray.to_luma8());
+        let thres = threshold(&gray.to_luma8(), otsu, ThresholdType::Binary);
+        let morph = grayscale_open(&thres, &Mask::square(3));
 
-        // let otsu = otsu_level(gray);
-        // println!("{}", otsu);
-        // let thres = threshold(gray, 200, ThresholdType::Binary);
-
-        // let morph = grayscale_open(&thres, &Mask::square(3));
-        let edges = canny(&self.image.to_luma8(), 50.0, 100.0);
+        let edges = canny(&morph, 50.0, 100.0);
 
         let options = LineDetectionOptions {
-            vote_threshold: 170,
+            vote_threshold: 250,
             suppression_radius: 90,
         };
 
         let lines: Vec<PolarLine> = detect_lines(&edges, options);
 
-        let horizontal_lines: Vec<PolarLine> = lines
-            .iter()
-            .filter(|&&x| x.angle_in_degrees.ge(&85) && x.angle_in_degrees.le(&95))
-            .cloned()
-            .collect();
+        // let horizontal_lines: Vec<PolarLine> = lines
+        //     .iter()
+        //     .filter(|&&x| x.angle_in_degrees.ge(&85) && x.angle_in_degrees.le(&95))
+        //     .cloned()
+        //     .collect();
 
         let mut vertical_lines: Vec<PolarLine> = lines
             .iter()
@@ -60,14 +63,101 @@ impl ImageProc for Reciept {
             .cloned()
             .collect();
 
-        vertical_lines.extend(horizontal_lines);
+        vertical_lines.sort_by_key(|x| x.r.round() as i64);
+        // vertical_lines.extend(horizontal_lines);
 
-        println!("{:?}", vertical_lines);
+        
 
-        let green = Rgb::<u8>([0, 255, 0]);
-        // Draw lines on top of edge image
-        let lines_image = draw_polar_lines(&gray.clone().into_rgb8(), &vertical_lines, green);
+        // will need to error handle a lot here
+        let a = Self::intersection_points(*vertical_lines.last().unwrap(),self.image.width(),self.image.height()).unwrap();
+        let b = Self::intersection_points(*vertical_lines.first().unwrap(),self.image.width(),self.image.height()).unwrap();
+        let right = cmp::max(a.0.0.round() as u64, a.1.0.round() as u64);
+        let left = cmp::min(b.0.0.round() as u64, b.1.0.round() as u64);
+        let height = self.image.height();
 
-        self.image = image::DynamicImage::ImageRgb8(lines_image);
+        
+        let answer = imageops::crop(&mut self.image, left as u32, 0, (right-left) as u32, height);
+
+        self.image = image::DynamicImage::ImageRgba8(answer.to_image());
+
+        //debug stuff
+        // let green = Rgb::<u8>([0, 255, 0]);
+        // let lines_image = draw_polar_lines(&gray.clone().into_rgb8(), &[*vertical_lines.last().unwrap(),*vertical_lines.first().unwrap()], green);
+        // let _ = lines_image.save("temp.jpg");
+
     }
+
+    // temp
+    fn intersection_points(
+    line: PolarLine,
+    image_width: u32,
+    image_height: u32,
+) -> Option<((f32, f32), (f32, f32))>{
+    let r = line.r;
+    let m = line.angle_in_degrees;
+    let w = image_width as f32;
+    let h = image_height as f32;
+
+    // Vertical line
+    if m == 0 {
+        return if r >= 0.0 && r <= w {
+            Some(((r, 0.0), (r, h)))
+        } else {
+            None
+        };
+    }
+
+    // Horizontal line
+    if m == 90 {
+        return if r >= 0.0 && r <= h {
+            Some(((0.0, r), (w, r)))
+        } else {
+            None
+        };
+    }
+
+    let theta = (m as f32).to_radians();
+    let (sin, cos) = theta.sin_cos();
+
+    let right_y = cos.mul_add(-w, r) / sin;
+    let left_y = r / sin;
+    let bottom_x = sin.mul_add(-h, r) / cos;
+    let top_x = r / cos;
+
+    let mut start = None;
+
+    if right_y >= 0.0 && right_y <= h {
+        let right_intersect = (w, right_y);
+        if let Some(s) = start {
+            return Some((s, right_intersect));
+        }
+        start = Some(right_intersect);
+    }
+
+    if left_y >= 0.0 && left_y <= h {
+        let left_intersect = (0.0, left_y);
+        if let Some(s) = start {
+            return Some((s, left_intersect));
+        }
+        start = Some(left_intersect);
+    }
+
+    if bottom_x >= 0.0 && bottom_x <= w {
+        let bottom_intersect = (bottom_x, h);
+        if let Some(s) = start {
+            return Some((s, bottom_intersect));
+        }
+        start = Some(bottom_intersect);
+    }
+
+    if top_x >= 0.0 && top_x <= w {
+        let top_intersect = (top_x, 0.0);
+        if let Some(s) = start {
+            return Some((s, top_intersect));
+        }
+    }
+
+    None
+}
+
 }
--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,18 @@ use grocery_to_json::{aldi::Aldi, imageproc::ImageProc, reciept::Reciept, tesser
 use image::ImageReader;
 
 fn main() {
-    let mut test: Reciept = ImageReader::open("./test6.png")
+    let mut test: Reciept = ImageReader::open("./test5.png")
         .unwrap()
         .try_into()
         .unwrap();
 
-    test.otsu();
-
-    // let mut binding = test.image.clone();
-    // let answer = imageops::crop(&mut binding, 0, 0, test.image.width()/2, test.image.height()/2);
-    // let _ = answer.to_image().save("crop.png");
-
+    
+    test.crop_gray();
+    test.otsu(1);
     let _ = test.image.save("sample.png");
-    test.apply();
 
+
+    test.apply();
     println!("{}", test.text);
 
     let wow: Aldi = test.try_into().unwrap();
